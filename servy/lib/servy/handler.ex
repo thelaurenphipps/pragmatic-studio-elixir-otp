d042232edfd62564b80e1463d23c54817360da91
Lauren Phipps <thelaurenphipps@gmail.com>
2022-11-28 11:24:48 -0600

    end of lesson 6

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,10 +1,13 @@
 defmodule Servy.Handler do
     def handle(request) do
         request 
-        |> parse 
-        |> route 
+        |> parse
+        |> log
+        |> route
         |> format_response
     end
+
+    def log(conv), do: IO.inspect conv
 
     def parse(request) do
         [method, path, _] = 
@@ -17,7 +20,15 @@ defmodule Servy.Handler do
     end
 
     def route(conv) do
+        route(conv, conv.method, conv.path)
+    end
+
+    def route(conv, "GET", "/wildthings") do 
         %{ conv | resp_body: "Bears, Lions, Tigers" }
+    end
+
+    def route(conv, "GET", "/bears") do 
+       %{ conv | resp_body: "Teddy, Smokey, Paddington" }
     end
 
     def format_response(conv) do
@@ -38,6 +49,26 @@ User-Agent: ExampleBrowser/1.0
 Accept */*
 
 """
+response = Servy.Handler.handle(request)
+IO.puts response
 
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept */*
+
+"""
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept */*
+
+"""
 response = Servy.Handler.handle(request)
 IO.puts response
